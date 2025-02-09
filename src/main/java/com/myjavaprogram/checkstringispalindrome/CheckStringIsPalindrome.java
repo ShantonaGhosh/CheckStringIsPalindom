76912709b20bc37e340b6067dfbe5b1338a58d8e
ShantonaGhosh <58144549+ShantonaGhosh@users.noreply.github.com>
2025-02-09 22:21:20 +0600

    Check string is palindrom

---
+++ src/main/java/com/myjavaprogram/checkstringispalindrome/CheckStringIsPalindrome.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myjavaprogram.checkstringispalindrome;
+
+/**
+ *
+ * @author artificial-softair
+ */
+public class CheckStringIsPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
